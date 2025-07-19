b9d78df11f238e08a6498de1f8c16972c0490f9d
ABHINAV RAJ <115422150+jumperraj@users.noreply.github.com>
2025-07-19 14:52:17 +0530

    Time: 8 ms (59.62%) | Memory: 80.1 MB (14.42%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -6,34 +6,27 @@ class Solution {
         for(int i =0 ; i< m ; i++){
             for(int j=0; j< n ;j++)
                 res[i][j] = grid[n-1-j][i];
-            // System.out.println("i "+i);
         }
        for(int j = 0 ;j< n;j++){
         int i = m-1;
         int count=0;
+        int jind =0;
         while(i>=0){
-            int jind = i;
+            jind = i;
             while(i>=0&&res[i][j]!='*'){
-            // System.out.println("res[i][j] "+res[i][j]+" i "+i+" j "+j);
                 if(res[i][j]=='#'){
                 count++;
                 res[i][j]='.';
-                // System.out.println("s001");
             }
             i--;
         }
         i--;
-        // if(count==0){
-        //     continue;
-        // }
         while(count>0&&jind>=0){
             res[jind][j]='#';
             count--;
             jind--;
-            // System.out.println("s002");
         }
-        // System.out.println("s003 j"+j+" i "+i);
-        }
+    }
        }
         return res;
     }
