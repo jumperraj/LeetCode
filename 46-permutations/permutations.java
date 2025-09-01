3aa8bab5f251455a078e4dbc273587728a0bf5ba
ABHINAV RAJ <115422150+jumperraj@users.noreply.github.com>
2025-09-01 19:24:26 +0530

    Time: 1 ms (92.67%) | Memory: 44.3 MB (97.27%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,29 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class Solution{ public static List<List<Integer>> permute(int[] nums) {
-    List<List<Integer>> lr = new ArrayList<>();
-    List<Integer> cur = new ArrayList<>();
-    boolean[] v = new boolean[nums.length];
-    solve(nums,v,cur,lr);
-    return lr;
-}
-static void solve(int[] nums, boolean[] v, List<Integer> cur, List<List<Integer>> lr)
-{
-    if(cur.size()==nums.length)
-    {
-        lr.add(new ArrayList<>(cur));
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> lr = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        boolean[] v = new boolean[nums.length];
+        solve(lr,cur,nums,v);
+        return lr;
     }
-  for(int i=0;i<nums.length;i++)
-  {
-      if(v[i])
-          continue;
-      cur.add(nums[i]);
-      v[i]=true;
-      solve(nums,v,cur,lr);
-      v[i]=false;
-      cur.remove(cur.size()-1);
-
-  }
-}
+    void solve(List<List<Integer>> lr , List<Integer> cur , int[] nums , boolean[] v ){
+        if(cur.size()==nums.length){
+            lr.add(new ArrayList(cur));
+        }
+        for(int i =0; i<nums.length;i++){
+            if(v[i]) continue;
+            cur.add(nums[i]);
+            v[i]=true;
+            solve(lr,cur,nums,v);
+            v[i]=false;
+            cur.remove(cur.size()-1);
+        }
+    }
 }
